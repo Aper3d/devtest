7da1684ff5d7eecebaba614d636425dbc0b0194e
Oleg Dmitriev <oleg_surgut@hotmail.com>
2025-12-25 21:25:45 +0500

    1com

---
+++ README.md
@@ -0,0 +1,6 @@
+# This project demonstrates a simple DevOps setup using Docker Compose
+
+- Node.js backend servise
+- PostgresSQL database with presistent volume
+- Environment variables via .env
+- Healthcheck endpoint for service and datababse
--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express = require('express');
+const { Pool } = require('pg');
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: 5432,
 });
 
-app.get('/health', (req, res) => {
-    res.status(200).json({ status: 'OK' });
+app.get('/', (req, res) => {
+    res.send('Hello from Express.js with PostgreSQL!');
+});
+
+app.get('/health', async (req, res) => {
+    try {
+        await pool.query('SELECT 1');
+        res.status(200).json({ status: 'ok', db: 'connected' });
+    } catch (error) {
+        res.status(500).json({ status: 'error', db: 'disconnected' });
+    }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on ${PORT}`);
 });
